c006efc5c77b8e7cf5b780fb57a1a2a79b5c5c4d
btt46 <tuanthanhbui46@gmail.com>
2021-12-05 22:13:49 +0900

    refactor

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -35,7 +35,7 @@ read -p "Do you use iterative back translation (Y/N): " IBT
 echo "$IBT"
 
 if [ $IBT != "N" ]; then
-    read -p "Which steps do you train: " $STEP
+    read -p "Which steps do you train: " STEP
     IBT_DATASET=DATASET=$EXPDIR/dataset/ibt_step_${STEP}
     DATA=$IBT_DATASET/data
     PROCESSED_DATA=$IBT_DATASET/processed
