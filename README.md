e893a31486948c6da4def142e4bd38289d95f7f8
btt46 <tuanthanhbui46@gmail.com>
2021-12-05 20:39:26 +0900

    first uplaod

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # bidirectional-translation
+
+### Setup an environment
+
+```
+sh setup-environment.sh
+```
+
+### Dataset
+
+Here, we use IWSLT (2015)) English-Vietnamese dataset.
+
+To download the dataset, run the command below
+
+```
+sh scripts/download-data.sh
+```
---
+++ setup-environment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+EXPDIR=$PWD
+
+# mosesdecoder
+if [ ! -d $EXPDIR/mosesdecoder ]; then
+    echo 'Cloning Moses github repository (for tokenization scripts)...'
+    git clone https://github.com/moses-smt/mosesdecoder.git
+fi
+
+# fairseq
+if [ ! -d $EXPDIR/fairseq ]; then 
+    echo 'Cloning fairseq repository (for training models)...'
+    git clone https://github.com/pytorch/fairseq
+    cd fairseq
+    pip3.6 install --editable ./
+    cd ../
+fi
+
+
+# subword-nmt 
+
+echo 'Cloning Subword NMT repository (for BPE pre-processing)...'
+git clone https://github.com/rsennrich/subword-nmt.git
+# pip3.6 install subword-nmt
