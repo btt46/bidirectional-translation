55856fb93c2b528d6c9516cf13c84536ef74f963
btt46 <tuanthanhbui46@gmail.com>
2021-12-05 21:59:40 +0900

    preproces

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
-
-SRCS="en vi"
-TGTS="vi en"
-BPESIZE=5000
-
 EXPDIR=$PWD 
+
+BPESIZE=5000
 
 # Libraries and Framework
 MOSES=$EXPDIR/mosesdecoder/scripts
@@ -16,22 +13,180 @@ TRUECASER_TRAIN=$MOSES/recaser/train-truecaser.perl
 TRUECASER=$MOSES/recaser/truecase.perl
 FARISEQ=$PWD/fairseq
 
+# LOGS
+LOGS=$EXPDIR/logs
+
+if [ ! -d $LOGS ]; then
+    mkdir -p $LOGS
+fi
+
+
+
 # Data
 DATASET=$EXPDIR/dataset
 RAW_DATA=$DATASET/iwslt15
-DATA=$DATASET/data
-PROCESSED_DATA=$DATASET/processed-data
-NORMALIZED_DATA=$DATASET/normalized
-TOKENIZED_DATA=$DATASET/tok
-TRUECASED_DATA=$DATASET/truecased
-BPE_DATA=$DATASET/bpe-data
-BIN_DATA=$DATASET/bin-data
+
+# BPE models
+BPE_MODEL=$DATASET/bpe-model
+
+STEP=-1
+IBT="N"
+read -p "Do you use iterative back translation (Y/N): " $IBT
+
+if [ "${IBT}" = "Y" ] || [ "${IBT}" = "y" ] || [ "${IBT}" = "yes" ] || [ "${IBT}" = "Yes" ]; then
+    read -p "Which steps do you train: " $STEP
+    IBT_DATASET=DATASET=$EXPDIR/dataset/ibt_step_${STEP}
+    DATA=$IBT_DATASET/data
+    PROCESSED_DATA=$IBT_DATASET/processed
+    NORMALIZED_DATA=$IBT_DATASET/normalized
+    TOKENIZED_DATA=$IBT_DATASET/tok
+    TRUECASED_DATA=$IBT_DATASET/truecased
+    BPE_DATA=$IBT_DATASET/bpe-data
+    BIN_DATA=$IBT_DATASET/bin-data
+else
+    DATA=$DATASET/data
+    PROCESSED_DATA=$DATASET/processed
+    NORMALIZED_DATA=$DATASET/normalized
+    TOKENIZED_DATA=$DATASET/tok
+    TRUECASED_DATA=$DATASET/truecased
+    BPE_DATA=$DATASET/bpe-data
+    BIN_DATA=$DATASET/bin-data
+fi
 
 DATA_NAME="train valid test"
 
 # scripts
 SCRIPTS=$EXPDIR/scripts
 
-TEXT_UTILS=$EXPDIR/utils
+# UTILS
+UTILS=$EXPDIR/utils
+
+##### PREPROCESSING
+echo "PREPROCESSING"
+echo "=> refreshing..."
+rm -rf $DATA
+rm -rf $PROCESSED_DATA
+rm -rf $NORMALIZED_DATA
+rm -rf $TOKENIZED_DATA
+rm -rf $TRUECASED_DATA
+rm -rf $BPE_MODEL
+rm -rf $BPE_DATA
+rm -rf $BIN_DATA
+
+mkdir -p $DATA
+mkdir -p $PROCESSED_DATA
+mkdir -p $NORMALIZED_DATA
+mkdir -p $TOKENIZED_DATA
+mkdir -p $TRUECASED_DATA
+
+echo "=> refresh done!"
+
+## Create train, dev, test dataset
+echo "=> Creating train, dev, test dataset"
+for lang in en vi; do
+    cp ${RAW_DATA}/train.${lang} ${DATA}/train.${lang}
+    cp ${RAW_DATA}/tst2012.${lang} ${DATA}/valid.${lang}
+    cp ${RAW_DATA}/tst2013.${lang} ${DATA}/test.${lang}
+done
+
+# normalization
+echo "=> normalizing..."
+for lang in en vi; do
+    echo "[$lang]..."
+    for set in $DATA_NAME; do
+        echo "$set..."
+        python3.6 ${UTILS}/normalize.py ${DATA}/${set}.${lang}  ${NORMALIZED_DATA}/${set}.${lang}
+    done
+done
+
+# Tokenization
+echo "=> tokenize..."
+for SET in $DATA_NAME ; do
+    $TOK -l en < ${NORMALIZED_DATA}/${SET}.en > ${TOKENIZED_DATA}/${SET}.en
+    python3.6 ${UTILS}/tokenize-vi.py  ${NORMALIZED_DATA}/${SET}.vi ${TOKENIZED_DATA}/${SET}.vi
+done
+
+# Truecaser
+echo "=> Truecasing..."
+
+echo "Traning for english..."
+$TRUECASER_TRAIN --model truecase-model.en --corpus ${TOKENIZED_DATA}/train.en
+
+echo "Traning for vietnamese..."
+$TRUECASER_TRAIN --model truecase-model.vi --corpus ${TOKENIZED_DATA}/train.vi
+
+for lang in en vi; do
+    echo "[$lang]..."
+    for set in $DATA_NAME; do
+        echo "${set}..."
+        $TRUECASER --model truecase-model.${lang} < ${TOKENIZED_DATA}/${set}.${lang} > ${TRUECASED_DATA}/${set}.${lang}
+    done
+done
 
 
+if [ $STEP -gt -1 ]; then
+    # prepare data for the bidirectional model
+    echo "=> Merge data for the bidirectional model"
+    for SET in $DATA_NAME ; do
+        touch ${PROCESSED_DATA}/${SET}.src
+        touch ${PROCESSED_DATA}/${SET}.tgt
+        python3.6 ${UTILS}/merge-file.py  \
+                                        -s1 ${TRUECASED_DATA}/${SET}.en\
+                                        -s2 ${TRUECASED_DATA}/${SET}.vi\
+                                        -s3 ${PROCESSED_DATA}/${SET}.src\
+                                        -msrc ${PROCESSED_DATA}/${SET}.src \
+                                        -t1 ${TRUECASED_DATA}/${SET}.en\
+                                        -t2 ${TRUECASED_DATA}/${SET}.vi\
+                                        -t3 ${PROCESSED_DATA}/${SET}.tgt\
+                                        -mtgt ${PROCESSED_DATA}/${SET}.tgt \
+                                        -t "sentence" -stride 0
+    done
+
+    echo "=> merged"
+
+    # learn bpe model with training data
+    if [ ! -d $BPE_MODEL ]; then  
+        mkdir -p $BPE_MODEL
+    fi
+
+    echo "=> LEARNING BPE MODEL: $BPE_MODEL"
+    subword-nmt learn-joint-bpe-and-vocab --input ${PROCESSED_DATA}/train.src ${PROCESSED_DATA}/train.tgt \
+                    -s $BPESIZE -o $BPE_MODEL/code.${BPESIZE}.bpe \
+                    --write-vocabulary $BPE_MODEL/train.src.vocab $BPE_MODEL/train.tgt.vocab 
+
+fi
+
+# apply sub-word segmentation
+echo "=> Apply sub-word"
+if [ ! -d $BPE_DATA ]; then
+    mkdir $BPE_DATA
+fi
+
+for SET in $DATA_NAME; do
+    subword-nmt apply-bpe -c $BPE_MODEL/code.${BPESIZE}.bpe < ${PROCESSED_DATA}/${SET}.src > $BPE_DATA/${SET}.src 
+    subword-nmt apply-bpe -c $BPE_MODEL/code.${BPESIZE}.bpe < ${PROCESSED_DATA}/${SET}.tgt > $BPE_DATA/${SET}.tgt
+done
+
+echo "=> Done"
+
+echo "=> Add tags"
+
+for SET in $DATA_NAME; do
+    python3.6 $UTILS/addTag.py -f $BPE_DATA/${SET}.src -p1 1 -t1 "<e2v>" -p2 2 -t2 "<v2e>" 
+done
+
+echo "=> Done"
+
+# binarize train/valid/test
+echo "=> Binarize"
+if [ ! -d $BIN_DATA ]; then
+    mkdir $BIN_DATA
+fi
+fairseq-preprocess -s src -t tgt \
+			--destdir $BIN_DATA \
+			--trainpref $BPE_DATA/train \
+			--validpref $BPE_DATA/valid \
+			--testpref $BPE_DATA/test \
+			--joined-dictionary \
+			--workers 32 \
+            2>&1 | tee $LOG/preprocess/log.preprocess.${IBT}.${STEP}
---
+++ utils/merge-file.py
@@ -0,0 +1,54 @@
+import argparse
+
+def merge_file(file_1, file_2, file_3,stride,out_file,type):
+    file_content_1 = [] 
+    file_content_2 = []
+    file_content_3 = []
+
+    with open(file_1,"r",encoding="utf-8") as fp1:
+        file_content_1 = fp1.readlines()
+        fp1.close()
+
+    with open(file_2,"r",encoding="utf-8") as fp2:
+        file_content_2 = fp2.readlines()
+        fp2.close()
+
+    with open(file_3,"r",encoding="utf-8") as fp3:
+        file_content_3 = fp3.readlines()
+        fp3.close()
+
+    if len(file_content_1) != len(file_content_2):
+        print("2 files have not the same size")
+        exit()
+    
+    with open(out_file,"a",encoding="utf-8") as fp:
+        if type == "sentence":
+            for i in range(len(file_content_1)):
+                fp.write(file_content_1[i])
+                fp.write(file_content_2[i])
+                fp.write(file_content_3[i:i+stride])
+        if type == "all":
+            fp.write(file_content_1)
+            fp.write(file_content_2)
+            fp.write(file_content_3)
+        fp.close()
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='ソースファイルを作る')
+    parser.add_argument("-s1", "--source_1",help="ファイル名を入力してください")
+    parser.add_argument("-s2", "--source_2",help="ファイル名を入力してください")
+    parser.add_argument("-s3", "--source_3",help="ファイル名を入力してください")
+    parser.add_argument("-msrc", "--merge_source", help="生成ファイル名")
+
+    parser.add_argument("-t1", "--target_1",help="ファイル名を入力してください")
+    parser.add_argument("-t2", "--target_2",help="ファイル名を入力してください")
+    parser.add_argument("-t3", "--target_3",help="ファイル名を入力してください")
+    parser.add_argument("-mtgt", "--merge_target", help="生成ファイル名")
+
+    parser.add_argument("-t", "--type",help='"all" or "sentence"')
+    parser.add_argument("-stride", "--stride",help='stride')
+
+    args = parser.parse_args() 
+
+    merge_file(args.source_1, args.source_2, args.source_3, args.stride, args.merge_source,args.type)
+    merge_file(args.target_1, args.target_2, args.target_3, args.stride, args.merge_target,args.type)
