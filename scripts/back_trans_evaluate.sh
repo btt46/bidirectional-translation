fa732783089cb0962e51f2a82dc74cb23962f513
btt46 <tuanthanhbui46@gmail.com>
2021-12-08 20:01:44 +0900

    refactor

--- scripts/back_trans_evaluate.sh
+++ scripts/back_trans_evaluate.sh
@@ -21,22 +21,31 @@ BLEU=$EXPDIR/multi-bleu.pl
 
 read -p "beam or random: " TRANSLATION_TYPE
 SYN_DATA=$DATASET/ibt_step_${STEP}_${TRANSLATION_TYPE}/synthetic-data
-REF_EN=$DATASET/ibt_step_${STEP}_${TRANSLATION_TYPE}/data/train.en
-REF_VI=$DATASET/ibt_step_${STEP}_${TRANSLATION_TYPE}/data/train.vi
+# REF_EN=$DATASET/ibt_step_${STEP}_${TRANSLATION_TYPE}/data/train.en
+# REF_VI=$DATASET/ibt_step_${STEP}_${TRANSLATION_TYPE}/data/train.vi
+
+
 
 RESULT_FOLDER=$BACK_EVALUATE/ibt_step_${STEP}_${TRANSLATION_TYPE}
+
 
 if [ ! -d $RESULT_FOLDER ]; then
     mkdir -p $RESULT_FOLDER
 fi
 
-
+REF_EN=$RESULT_FOLDER/ref.en
+REF_VI=$RESULT_FOLDER/ref.vi
 HYP_EN=$RESULT_FOLDER/hyp.en
 HYP_VI=$RESULT_FOLDER/hyp.vi
 
+cat $DATASET/translation-data/train.en | sed -r 's/(@@ )|(@@ ?$)//g' > $REF_EN
+cat $DATASET/translation-data/train.vi | sed -r 's/(@@ )|(@@ ?$)//g' > $REF_VI
 
-$DETRUECASER < $SYN_DATA/syn.en > $HYP_EN
-$DETRUECASER < $SYN_DATA/syn.vi > $HYP_VI
+cat $SYN_DATA/syn.en > $HYP_EN
+cat $SYN_DATA/syn.vi > $HYP_VI
+
+# $DETRUECASER < $SYN_DATA/syn.en > $HYP_EN
+# $DETRUECASER < $SYN_DATA/syn.vi > $HYP_VI
 
 env LC_ALL=en_US.UTF-8 perl $BLEU $REF_VI < $HYP_VI > $RESULT_FOLDER/bleu.vi
 env LC_ALL=en_US.UTF-8 perl $BLEU $REF_EN < $HYP_EN > $RESULT_FOLDER/bleu.en
