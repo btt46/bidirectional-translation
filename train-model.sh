3cdc5d16fc110a026a01ebec17336762673884bd
btt46 <tuanthanhbui46@gmail.com>
2021-12-14 03:33:58 +0900

    update

--- train-model.sh
+++ train-model.sh
@@ -48,6 +48,36 @@ if [ ${STEP} -eq -1 ]; then
 
 fi
 
+if [ ${STEP} -eq -2 ]; then
+	read -p "Source language (en or vi): " SRC
+	read -p "Target language (en or vi): " TGT
+	UNI_DATASET=$EXPDIR/dataset/${SRC}-${TGT}/bin-data
+	CUDA_VISIBLE_DEVICES=$GPUS fairseq-train $UNI_DATASET -s ${SRC} -t ${TGT} \
+		            --log-interval 100 \
+					--log-format json \
+					--max-epoch ${EPOCHS} \
+		    		--optimizer adam --lr 0.0001 \
+					--clip-norm 0.0 \
+					--max-tokens 4000 \
+					--no-progress-bar \
+					--log-interval 100 \
+					--min-lr '1e-09' \
+					--weight-decay 0.0001 \
+					--criterion label_smoothed_cross_entropy \
+					--label-smoothing 0.1 \
+					--lr-scheduler inverse_sqrt \
+					--warmup-updates 4000 \
+					--warmup-init-lr '1e-08' \
+					--adam-betas '(0.9, 0.98)' \
+					--arch transformer_iwslt_de_en \
+					--dropout 0.1 \
+					--attention-dropout 0.1 \
+					--share-decoder-input-output-embed \
+					--save-dir $MODELS/$MODEL_NAME \
+					2>&1 | tee $LOG/${MODEL_NAME}
+
+fi
+
 
 if [ ${STEP} -eq 0 ]; then
     echo "=>> Training a bidirectional model..."
